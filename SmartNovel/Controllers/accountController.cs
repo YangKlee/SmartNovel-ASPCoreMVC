eef683527ae1dd2202726b2a47fec31f61d0d668
YangKlee <147032564+YangKlee@users.noreply.github.com>
2026-05-30 17:26:29 +0700

    backup

---
+++ SmartNovel/Controllers/accountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using SmartNovel.Models;
+using SmartNovel.Models.ViewModel;
+using SmartNovel.Services;
+using System.Security.Claims;
+
+namespace SmartNovel.Controllers
+{
+    public class accountController : Controller
+    {
+        private readonly SmartTruyenDbContext _context;
+        private readonly MailServices _mailServices;
+        private readonly IMemoryCache _cache;
+
+        public accountController(SmartTruyenDbContext context, MailServices mailServices, IMemoryCache cache)
+        {
+            _mailServices = mailServices;
+            _context = context;
+            _cache = cache;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var uid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            
+            if (uid == null)
+            {
+                return Redirect("auth/Login");
+            }
+            var model = await _context.Users.FirstOrDefaultAsync(x => x.Uid == uid);
+            return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> updateInfo()
+        {
+            var uid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (uid == null)
+            {
+                return Redirect("auth/Login");
+            }
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Uid == uid);
+            var model = new UserProfileViewModel();
+            model.Phone = user.Phone;
+            model.Birthday = user.Birthday;
+            model.DisplayName = user.DisplayName;
+            return View("ChangeInfo", model);
+        }
+    }
+}
---
+++ SmartNovel/Models/ViewModel/UserProfileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartNovel.Models.ViewModel
+{
+    public class UserProfileViewModel
+    {
+            [Required(ErrorMessage = "Tên hiển thị không được để trống")]
+            [StringLength(50, ErrorMessage = "Tên hiển thị không quá 50 ký tự")]
+            [Display(Name = "Tên hiển thị")]
+            public string DisplayName { get; set; }
+
+            [Required(ErrorMessage = "Vui lòng chọn ngày sinh")]
+            [DataType(DataType.Date)]
+            [Display(Name = "Ngày sinh")]
+            public DateOnly? Birthday { get; set; }
+
+            [Required(ErrorMessage = "Số di động không được để trống")]
+            [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
+            [RegularExpression(@"^(0[3|5|7|8|9])+([0-8]{8})\b$", ErrorMessage = "Số điện thoại không đúng định dạng Việt Nam")]
+            [Display(Name = "Di động")]
+            public string Phone { get; set; }
+
+    }
+}
