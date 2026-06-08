76562e90d2e11bb272d406840547c5938afed4da
YangKlee <147032564+YangKlee@users.noreply.github.com>
2026-06-09 01:39:05 +0700

    backup

---
+++ SmartNovel/Views/Shared/Components/CommentComponent/CommentViewComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartNovel.Views.Shared.Components.CommentComponent
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SmartNovel.Models;
+
+    public class CommentViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(Comment model)
+        {
+            return View(model);
+        }
+    }
+}
