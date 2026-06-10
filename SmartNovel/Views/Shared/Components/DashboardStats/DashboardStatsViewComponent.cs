f850f3784630ed5b6000c8dd6c87c5510758c609
YangKlee <147032564+YangKlee@users.noreply.github.com>
2026-06-10 22:48:44 +0700

    backup

---
+++ SmartNovel/Views/Shared/Components/DashboardStats/DashboardStatsViewComponent.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartNovel.Models;
+using SmartNovel.Models.ViewModels;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SmartNovel.Views.Shared.Components.DashboardStats
+{
+    public class DashboardStatsViewComponent : ViewComponent
+    {
+
+
+        public async Task<IViewComponentResult> InvokeAsync(DashboardStats )
+        {
+ 
+        }
+    }
+}
